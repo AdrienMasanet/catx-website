ae5b80692f0710ddb402ee0d656a360b59584eae
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-04-07 14:34:14 +0200

    chore: configure Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "catx-red": "#970502",
+      "catx-yellow": "#fee300",
+      "catx-black": "#171716",
+      "catx-white": "#fafafa",
+    },
+    extend: {
+      fontFamily: {
+        bignoodletitling: ["var(--font-bignoodletitling)"],
+        raleway: ["var(--font-raleway)"],
+      },
+      maxWidth: {
+        "9/10": "90%",
+      },
+      transitionTimingFunction: {
+        "catx-ease": "cubic-bezier(0.08, 0.04, 0, 1)",
+      },
+    },
   },
   plugins: [],
 };
