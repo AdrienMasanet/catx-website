ece4375fcb455fd0b6694fec16cda73929c891ed
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-04-15 22:23:44 +0200

    feat: only extend tailwind instead of replacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,13 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      "catx-red": "#970502",
-      "catx-yellow": "#fee300",
-      "catx-black": "#171716",
-      "catx-white": "#fafafa",
-    },
     extend: {
+      colors: {
+        "catx-red": "#970502",
+        "catx-yellow": "#fee300",
+        "catx-black": "#171716",
+        "catx-white": "#fafafa",
+      },
       fontFamily: {
         bignoodletitling: ["var(--font-bignoodletitling)"],
         raleway: ["var(--font-raleway)"],
@@ -20,6 +20,10 @@ module.exports = {
       },
       transitionTimingFunction: {
         "catx-ease": "cubic-bezier(0.08, 0.04, 0, 1)",
+      },
+      dropShadow: {
+        "catx-red-hard-sm": ".1em .1em 0 #970502",
+        "catx-red-hard-md": ".2em .2em 0 #970502",
       },
     },
   },
