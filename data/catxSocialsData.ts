8857f7bf193d960f73ac11d2b2b9fdb2aa0f802b
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-04-15 22:25:06 +0200

    feat: add static data

---
+++ data/catxSocialsData.ts
@@ -0,0 +1,26 @@
+import SocialMedia from "@/interfaces/SocialMedia";
+
+const catxSocialsData: SocialMedia[] = [
+  {
+    name: "Twitter",
+    url: "https://twitter.com/TeamCATx",
+    image: "/images/social/twitter.svg",
+  },
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com/TeamCATx",
+    image: "/images/social/facebook.svg",
+  },
+  {
+    name: "Youtube",
+    url: "https://www.youtube.com/@CATxTeam66",
+    image: "/images/social/youtube.svg",
+  },
+  {
+    name: "Twitch",
+    url: "https://www.twitch.tv/teamcatx",
+    image: "/images/social/twitch.svg",
+  },
+];
+
+export default catxSocialsData;
---
+++ data/teamsData.ts
@@ -0,0 +1,108 @@
+import Team from "@/interfaces/Team";
+
+const teamsData: Team[] = [
+  {
+    name: "Counter Strike : Global Offensive",
+    description: "Counter-Strike: Global Offensive is a first-person shooter video game developed by Hidden Path Entertainment and Valve Corporation. It is the fourth game in the Counter-Strike series and was released for Windows, OS X, Xbox 360, and PlayStation 3 in August 2012, while the Linux version was released in 2014.",
+    members: [
+      {
+        pseudo: "Beze",
+        name: "Thomas L.",
+        age: 21,
+        role: "Riffle",
+        image: "/images/teams/csgo.jpg",
+        socials: [
+          {
+            name: "Steam",
+            url: "https://steamcommunity.com/id/bezeeeee/",
+            image: "/images/social/steam.svg",
+          },
+          {
+            name: "Faceit",
+            url: "https://www.faceit.com/fr/players/beze_",
+            image: "/images/social/faceit.svg",
+          },
+        ],
+      },
+      {
+        pseudo: "Akarno",
+        name: "Sammi Z.",
+        age: 20,
+        role: "Riffle",
+        image: "/images/teams/csgo.jpg",
+        socials: [
+          {
+            name: "Steam",
+            url: "https://steamcommunity.com/id/AkarnoO",
+            image: "/images/social/steam.svg",
+          },
+          {
+            name: "Faceit",
+
+            url: "https://www.faceit.com/fr/players/AkarnOob",
+            image: "/images/social/faceit.svg",
+          },
+        ],
+      },
+      {
+        pseudo: "Magz",
+        name: "Yanic T.",
+        age: 21,
+        role: "AWP / LIG",
+        image: "/images/teams/csgo.jpg",
+        socials: [
+          {
+            name: "Steam",
+            url: "https://steamcommunity.com/profiles/76561198259827471",
+            image: "/images/social/steam.svg",
+          },
+          {
+            name: "Faceit",
+            url: "https://www.faceit.com/fr/players/MAGZj",
+            image: "/images/social/faceit.svg",
+          },
+        ],
+      },
+      {
+        pseudo: "Den",
+        name: "Andreas L.",
+        age: 33,
+        role: "Riffle",
+        image: "/images/teams/csgo.jpg",
+        socials: [
+          {
+            name: "Steam",
+            url: "https://steamcommunity.com/id/aNDREAS1",
+            image: "/images/social/steam.svg",
+          },
+          {
+            name: "Faceit",
+            url: "https://www.faceit.com/fr/players/dENNNNN_",
+            image: "/images/social/faceit.svg",
+          },
+        ],
+      },
+      {
+        pseudo: "Spiritix",
+        name: "Mathis F.",
+        age: 20,
+        role: "Riffle",
+        image: "/images/teams/csgo.jpg",
+        socials: [
+          {
+            name: "Steam",
+            url: "https://steamcommunity.com/id/spiritix",
+            image: "/images/social/steam.svg",
+          },
+          {
+            name: "Faceit",
+            url: "https://www.faceit.com/fr/players/Spirit1x",
+            image: "/images/social/faceit.svg",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default teamsData;
