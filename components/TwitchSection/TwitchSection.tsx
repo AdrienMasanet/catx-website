ca21c24afc0b0f1a1a8b9bffbb2e113524d5a6fb
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-04-15 22:30:07 +0200

    feat: add TwitchSection component

---
+++ components/TwitchSection/TwitchSection.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+import { TwitchEmbed } from "react-twitch-embed";
+import Section from "../Section/Section";
+import { hostname } from "os";
+
+const TwitchSection = () => {
+  const [isCatxLive, setIsCatxLive] = useState<boolean>(false);
+  const embed = useRef(); // Use a ref instead of state to avoid rerenders
+
+  const handleReady = (event: any) => {
+    embed.current = event;
+    console.log(event);
+  };
+
+  const checkIfCatxIsLive = async () => {
+    fetch("/api/catx-live-status", { method: "GET" })
+      .then((response) => {
+        if (!response.ok) throw new Error();
+        return response.json();
+      })
+      .then((data) => {
+        if (process.env.NODE_ENV === "development") console.log(data);
+        setIsCatxLive(data.isCatxLive);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    checkIfCatxIsLive();
+    console.log("PARENT", window.location.hostname);
+  }, []);
+
+  if (!isCatxLive) return null;
+
+  return (
+    <Section title={"Nous sommes en live"}>
+      <TwitchEmbed width={"100%"} channel={process.env.NEXT_PUBLIC_TWITCH_CHANNELNAME} autoplay withChat darkMode={true} onVideoReady={handleReady} />
+    </Section>
+  );
+};
+
+export default TwitchSection;
