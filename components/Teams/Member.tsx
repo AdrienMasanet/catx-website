5226d2b77698d97637710f7d990ba6fa60295dc2
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-04-15 23:47:08 +0200

    fix: do not use a heading for team member name

--- components/Teams/Member.tsx
+++ components/Teams/Member.tsx
@@ -12,7 +12,7 @@ const Member = ({ pseudo, image, reviewing }: MemberProps) => {
     <div className={`${styles.playercontainer} h-full cursor-pointer transition-all ease-catx-ease duration-500 ${reviewing ? "!opacity-100" : "opacity-20 hover:opacity-80"} -skew-x-12 select-none`}>
       <div className="relative flex flex-col h-full overflow-hidden">
         <div className="md:text-2xl border-t-2 border-catx-black mt-auto bg-catx-white">
-          <h4 className={`${styles.playername} ${reviewing ? "!bg-catx-yellow" : ""} lg:p-2 text-center font-bignoodletitling md:text-lg lg:text-2xl text-catx-black`}>{pseudo}</h4>
+          <p className={`${styles.playername} ${reviewing ? "!bg-catx-yellow" : ""} lg:p-2 text-center font-bignoodletitling md:text-lg lg:text-2xl text-catx-black`}>{pseudo}</p>
         </div>
         <Image className="-z-10 object-cover md:skew-x-12 overflow-visible" src={image} alt={`${pseudo} logo`} fill sizes="100%" />
       </div>
