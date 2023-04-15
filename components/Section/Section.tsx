15182abd0cd25795410223ab55d5289f04cf173c
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-04-15 23:19:11 +0200

    feat: change section title borders style

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -1,3 +1,5 @@
+import styles from "./Section.module.scss";
+
 type SectionProps = {
   title: string;
   id?: string;
@@ -7,7 +9,7 @@ type SectionProps = {
 const Section = ({ title, id, children }: SectionProps) => {
   return (
     <section className="pt-10 overflow-x-hidden" id={id}>
-      <h2 className="w-fit mx-auto pr-2 text-center font-bignoodletitling text-5xl text-catx-yellow italic border-2 border-catx-yellow border-l-0 border-r-0 select-none">{title}</h2>
+      <h2 className={`${styles.container} w-fit mx-auto text-center font-bignoodletitling text-5xl text-catx-yellow italic select-none`}>{title}</h2>
       <div className="mt-5 pb-20 p-5 font-raleway text-catx-white">{children}</div>
     </section>
   );
