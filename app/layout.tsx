3677d3515b45de669a7302e30b83126043b18736
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-04-15 23:20:50 +0200

    feat: actual layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,22 @@
-import "./globals.scss";
+import "@/styles/globals.scss";
+import localFont from "@next/font/local";
+import ScalesBackground from "@/components/ScalesBackground/ScalesBackground";
+
+const fontBigNoodleTitling = localFont({ src: "../public/fonts/big_noodle_titling.ttf", variable: "--font-bignoodletitling" });
+const raleway = localFont({ src: "../public/fonts/raleway.ttf", variable: "--font-raleway" });
 
 export const metadata = {
-  title: "CATx",
-  description: "description",
+  title: "Team CATx | Équipe e-sport française",
+  description: "Découvrez Team CATx, association eSport loi 1901 créée en 2023, évoluant sur CS:GO en ESEA OPEN S45 avec Akarno, Beze, deN, Magz & Spiritix. Passion, compétition, gaming et promotion de l'eSport. Som i serem.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="fr">
-      <body>{children}</body>
+    <html lang="fr" className={`${fontBigNoodleTitling.variable} ${raleway.variable}`}>
+      <body className="relative bg-catx-black">
+        {children}
+        <ScalesBackground />
+      </body>
     </html>
   );
 }
