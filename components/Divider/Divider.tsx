708ba2d511ecd74469213a83332c875d419b49c2
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-04-15 22:27:42 +0200

    feat: add Divider component

---
+++ components/Divider/Divider.tsx
@@ -0,0 +1,10 @@
+const Divider = () => {
+  return (
+    <>
+      <hr className="mx-auto mb-1 max-w-9/10 border-2 border-catx-yellow" />
+      <hr className="mx-auto max-w-9/10 border-2 border-catx-red" />
+    </>
+  );
+};
+
+export default Divider;
