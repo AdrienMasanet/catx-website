f8be6cccd23e507377c9e5a0be8e5895199eb8d0
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-04-18 20:29:04 +0200

    feat: add sizes attribute to each partner image

--- components/Partners/Partners.tsx
+++ components/Partners/Partners.tsx
@@ -14,7 +14,7 @@ const Partners = ({ partners }: PartnersProps) => {
           <div className="flex flex-col justify-center items-center mx-0 md:mx-5" key={partner.name}>
             <h4 className="w-fit mx-auto mb-2 md:mb-5 text-center font-bignoodletitling text-xl md:text-2xl text-catx-yellow select-none">{partner.name}</h4>
             <a className="relative w-20 h-20 md:w-40 md:h-40 transition-scale duration-300 hover:scale-105 ease-catx-ease" href={partner.defaultUrl} target="_blank" rel="noreferrer">
-              <Image className="object-cover" src={partner.image} alt={`${partner.name}, partenaire de la CATx`} fill />
+              <Image className="object-cover" src={partner.image} alt={`${partner.name}, partenaire de la CATx`} fill sizes="100%" />
             </a>
             <div className="grid justify-center grid-cols-3 lg:flex mt-5 gap-3 lg:gap-5">
               {partner.socials.map((social) => (
