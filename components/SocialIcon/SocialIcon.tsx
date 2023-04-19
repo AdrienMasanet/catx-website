5e2ccbb6e13d7867874038ea9cf6279df6b5c070
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-04-19 15:08:53 +0200

    fix: use anchor if social.url and div if social.textToCopyIntoClipboard

--- components/SocialIcon/SocialIcon.tsx
+++ components/SocialIcon/SocialIcon.tsx
@@ -9,6 +9,15 @@ type SocialIconProps = {
 };
 
 const SocialIcon = ({ social, scale = 100 }: SocialIconProps) => {
+  const commonProps = {
+    className: `relative transition-scale duration-300 hover:scale-105 ease-catx-ease cursor-pointer ${scale < 100 ? "drop-shadow-catx-red-hard-sm" : "drop-shadow-catx-red-hard-md"}`,
+    style: {
+      width: scale,
+      height: scale,
+    },
+    title: social.tooltip,
+  };
+
   function handleOnClick() {
     if (social.textToCopyIntoClipboard && navigator.clipboard) {
       navigator.clipboard.writeText(social.textToCopyIntoClipboard).then(() => {
@@ -17,22 +26,21 @@ const SocialIcon = ({ social, scale = 100 }: SocialIconProps) => {
     }
   }
 
-  return (
-    <a
-      className={`relative transition-scale duration-300 hover:scale-105 ease-catx-ease cursor-pointer ${scale! < 100 ? "drop-shadow-catx-red-hard-sm" : "drop-shadow-catx-red-hard-md"}`}
-      href={social.url}
-      target="_blank"
-      rel="noreferrer"
-      style={{
-        width: scale || 100,
-        height: scale || 100,
-      }}
-      onClick={handleOnClick}
-      title={social.tooltip}
-    >
-      <Image src={social.image} alt={`${social.name}`} fill />
-    </a>
-  );
+  if (social.url)
+    return (
+      <a {...commonProps} href={social.url} target="_blank" rel="noreferrer">
+        <Image src={social.image} alt={`${social.name}`} fill />
+      </a>
+    );
+
+  if (social.textToCopyIntoClipboard)
+    return (
+      <div {...commonProps} onClick={handleOnClick}>
+        <Image src={social.image} alt={`${social.name}`} fill />
+      </div>
+    );
+
+  return null;
 };
 
 export default SocialIcon;
