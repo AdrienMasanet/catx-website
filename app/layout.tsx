f5cf1cb802c0322e5ce681e9cfb6dc3ac918b1c3
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-04-27 16:49:13 +0200

    feat: add more meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,33 @@ const fontBigNoodleTitling = localFont({ src: "../public/fonts/big_noodle_titlin
 const raleway = localFont({ src: "../public/fonts/raleway.ttf", variable: "--font-raleway" });
 
 export const metadata = {
-  title: "Team CATx | Équipe e-sport française",
+  title: {
+    template: "Team CATx | %s",
+    default: "Team CATx | Équipe e-sport française",
+  },
   description: "Découvrez Team CATx, association eSport loi 1901 créée en 2023, évoluant sur CS:GO en ESEA OPEN S45 avec Akarno, Beze, deN, Magz & Spiritix. Passion, compétition, gaming et promotion de l'eSport. Som i serem.",
+  authors: [{ name: "Adrien Masanet", url: "https://adrienmasa.net" }],
+  creator: "Adrien Masanet",
+  publisher: "Team CATx",
+  colorScheme: "dark",
+  alternates: {
+    canonical: process.env.NEXT_PUBLIC_CANONICAL_URL,
+  },
+  openGraph: {
+    title: "Team CATx | Équipe e-sport française",
+    description: "Site officiel de Team CATx, association eSport loi 1901 créée en 2023. Passion, compétition, gaming et promotion de l'eSport. Som i serem.",
+    url: `${process.env.NEXT_PUBLIC_CANONICAL_URL}/`,
+    images: [
+      {
+        url: "images/catx_logo.png",
+        alt: "Team CATx logo",
+        width: 714,
+        height: 714,
+      },
+    ],
+    locale: "fr_FR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
